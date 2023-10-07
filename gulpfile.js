65b197ae580150df125bb46e4fe5da899e684850
Chinmay <chinz.sandur07@gmail.com>
2023-10-07 11:13:24 +0530

    kicing of the project

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
 const autoprefixer = require('gulp-autoprefixer');
 
 function css() {
     return gulp
         .src('scss/app.scss')
-        .pipe(autoprefixer() )
+        .pipe(autoprefixer())
         .pipe(sass({ outputStyle: "expanded" }))
         .pipe(gulp.dest("css"))
 }
